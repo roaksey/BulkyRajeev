01e9469bb405be7ed6b2986dc5d07ad141ad7f4b
RajeevKumarMahato <rajeev@teksewa.com>
2023-07-23 16:24:01 +0545

    test category create UI

--- BulkyRajeev/Controllers/CategoryController.cs
+++ BulkyRajeev/Controllers/CategoryController.cs
@@ -16,5 +16,10 @@ namespace BulkyRajeev.Controllers
             List<Category> categories = _db.Categories.ToList();
             return View(categories);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
