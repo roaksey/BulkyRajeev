f29a0b1540efcaaa7de0aa45501f91fe26042af6
RajeevKumarMahato <rajeev@teksewa.com>
2023-07-31 17:04:01 +0545

    Implementation of IcategoryRepository

---
+++ Bulky.DataAccess/Data/Repository/CategoryRepository.cs
@@ -0,0 +1,30 @@
+﻿using Bulky.DataAccess.Data.Repository.IReposiotry;
+using Bulky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Data.Repository
+{
+    internal class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private AppDbContext _db;
+        public CategoryRepository(AppDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Category obj)
+        {
+            _db.Update(obj);
+        }
+    }
+}
