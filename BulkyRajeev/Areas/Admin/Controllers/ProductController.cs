b5e8d00dd400156fa3ce519b6660cfaa6f864647
RajeevKumarMahato <rajeev@teksewa.com>
2023-08-02 12:14:18 +0545

    Create product controller and action method for crud operation of product

---
+++ BulkyRajeev/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,82 @@
+﻿using Bulky.DataAccess.Repository.IReposiotry;
+using Bulky.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            var products = _unitOfWork.Product.GetAll();
+            return View(products);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Product model)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Add(model);
+                _unitOfWork.Save();
+                TempData["success"] = "Product created successfully";
+                return RedirectToAction(nameof(Index)); 
+            }
+            return View(model);
+        }
+        public IActionResult Edit(int id) {
+            Product product = _unitOfWork.Product.GetFirstOrDefault(x=>x.Id  == id);
+            if (product != null)
+            {
+                return View(product);
+            }
+            return NotFound();
+        }
+        [HttpPost]
+        public IActionResult Edit(Product model)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Update(model);
+                _unitOfWork.Save();
+                TempData["success"] = "Product updated successfully";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(model);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            Product product = _unitOfWork.Product.GetFirstOrDefault(x => x.Id == id);
+            if (product != null)
+            {
+                return View(product);
+            }
+            return NotFound();
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int id)
+        {
+            Product product = _unitOfWork.Product.GetFirstOrDefault(x=>x.Id==id);   
+            if (product == null)
+            {                
+                return NotFound();
+            }
+            _unitOfWork.Product.Remove(product);
+            _unitOfWork.Save();
+            TempData["success"] = "Product deleted successfully";
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
