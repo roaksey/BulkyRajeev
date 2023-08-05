d0a7b0d085f87d9b68b5851ea95484e65b1a7cc9
RajeevKumarMahato <rajeev@teksewa.com>
2023-08-05 15:43:23 +0545

    Details UI with Details View added

--- BulkyRajeev/Areas/Customer/Controllers/HomeController.cs
+++ BulkyRajeev/Areas/Customer/Controllers/HomeController.cs
@@ -23,7 +23,10 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
             IEnumerable<Product> products = _unitOfWork.Product.GetAll();
             return View(products);
         }
-
+        public IActionResult Detail(int productId) {
+            Product product = _unitOfWork.Product.GetFirstOrDefault(x=>x.Id== productId,includeProperties:"Category");
+            return View(product);
+        }
         public IActionResult Privacy()
         {
             return View();
