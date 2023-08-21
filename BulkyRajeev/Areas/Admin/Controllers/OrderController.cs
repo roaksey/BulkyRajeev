006f721ec4f8e0b79a1002153f2edbf7114da71e
RajeevKumarMahato <rajeev@teksewa.com>
2023-08-21 11:28:00 +0545

    OrderVM and Order Controller

---
+++ BulkyRajeev/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,26 @@
+﻿using Bulky.DataAccess.Repository.IReposiotry;
+using Bulky.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Drawing;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        #region API calls
+        public IActionResult GetAll() {
+            List<OrderHeader> orderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "AppUser").ToList();
+            return Json(new { data = orderHeaders });
+        }
+        #endregion
+    }
+}
