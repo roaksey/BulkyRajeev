0b9b5e09f2f9b3dab6e897f9042a03e3aa0438c8
RajeevKumarMahato <rajeev@teksewa.com>
2023-08-22 16:02:10 +0545

    Section 12 - Ship Order

--- BulkyRajeev/Areas/Admin/Controllers/OrderController.cs
+++ BulkyRajeev/Areas/Admin/Controllers/OrderController.cs
@@ -11,6 +11,7 @@ using System.Security.Claims;
 namespace BulkyBookWeb.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize]
     public class OrderController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
@@ -60,6 +61,34 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
 
 
             return RedirectToAction(nameof(Detail), new { orderId = orderHeaderFromDb.Id });
+        }
+
+        [HttpPost]
+        [Authorize(Roles =SD.Role_Admin +","+SD.Role_Employee)]
+        public IActionResult StartProcessing()
+        {
+            _unitOfWork.OrderHeader.UpdateStatus(OrderVM.OrderHeader.Id, SD.StatusInProcess);
+            _unitOfWork.Save();
+            TempData["success"] = "Order Details updated successfully.";
+            return RedirectToAction(nameof(Detail), new { orderId = OrderVM.OrderHeader.Id });
+        }
+        [HttpPost]
+        [Authorize(Roles = SD.Role_Admin + "," + SD.Role_Employee)]
+        public IActionResult ShipOrder()
+        {
+            var orderHeader = _unitOfWork.OrderHeader.GetFirst(u=>u.Id == OrderVM.OrderHeader.Id);
+            orderHeader.TrackingNumber = OrderVM.OrderHeader.TrackingNumber;
+            orderHeader.Carrier = OrderVM.OrderHeader.Carrier;
+            orderHeader.OrderStatus = SD.StatusShipped;
+            orderHeader.ShippingDate = DateTime.Now;
+            if(orderHeader.PaymentStatus == SD.PaymentStatusDelayedPayment)
+            {
+                orderHeader.PaymentDueDate = DateTime.Now.AddDays(30);
+            }
+            _unitOfWork.OrderHeader.Update(orderHeader);
+            _unitOfWork.Save();
+            TempData["success"] = "Order shipped successfully.";
+            return RedirectToAction(nameof(Detail),new {orderId= OrderVM.OrderHeader.Id});  
         }
         #region API calls
         public IActionResult GetAll(string status) {
