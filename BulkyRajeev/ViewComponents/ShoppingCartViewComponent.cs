745b6fb6d273cb39b6b138c67dc96faeb9d2cd05
RajeevKumarMahato <rajeev@teksewa.com>
2023-09-01 08:47:32 +0545

    Section 13 - Create View Component

---
+++ BulkyRajeev/ViewComponents/ShoppingCartViewComponent.cs
@@ -0,0 +1,36 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository;
+using Bulky.Utility;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace BulkyBookWeb.ViewComponents
+{
+    public class ShoppingCartViewComponent : ViewComponent
+    {
+        private readonly UnitOfWork _unitOfWork;
+        public ShoppingCartViewComponent(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;   
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var claimIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            if(claim != null )
+            {
+                if(HttpContext.Session.GetInt32(SD.SessionCart) != null)
+                {
+                    HttpContext.Session.SetInt32(SD.SessionCart,_unitOfWork.ShoppingCart.GetAll(u=>u.AppUserId == claim.Value).Count());
+                }
+                return View(HttpContext.Session.GetInt32(SD.SessionCart));
+            }
+            else
+            {
+                HttpContext.Session.Clear();
+                return View(0);
+            }
+        }
+    }
+}
